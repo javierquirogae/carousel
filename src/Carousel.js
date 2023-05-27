ccbd72b93a10adaa30fca730f9860ab07ff546a6
jav <javier.quiroga.e@gmail.com>
2023-05-27 14:52:54 -0400

    fixed bug

--- src/Carousel.js
+++ src/Carousel.js
@@ -9,7 +9,22 @@ function Carousel(props) {
   const [cardIdx, setCardIdx] = useState(0);
   const card = props.cardData[cardIdx];
   const total = props.cardData.length;
-  const goForward = () => setCardIdx(cardIdx + 1);
+  
+  
+  const goForward = () =>  {
+    setCardIdx(cardIdx + 1);
+    if (cardIdx === total - 1) {
+      setCardIdx(0);
+    }
+  }
+  const goBackward = () => {
+    setCardIdx(cardIdx - 1);
+    if (cardIdx === 0) {
+      setCardIdx(total - 1);
+    }
+  }
+  // const goForward = () => setCardIdx(cardIdx + 1);
+  // const goBackward = () => setCardIdx(cardIdx - 1);
 
   return (
     <div className="Carousel">
@@ -17,7 +32,7 @@ function Carousel(props) {
       <div className="Carousel-main">
         <i
           className="fas fa-chevron-circle-left fa-2x"
-          onClick={goForward}
+          onClick={goBackward}
           data-testid="left-arrow"
         />
         <Card
